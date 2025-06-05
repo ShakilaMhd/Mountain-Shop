87dc53ffbb9fca1251cf5c9b636e6f8884e6bf50
ShakilaMhd <shakila1996mhd@gmail.com>
2025-06-05 20:07:35 +0330

    Add shopContext for use context

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,17 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import { product } from "../assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = ({ children }) => {
+
+  const currency = "$";
+
+  const [products, setProducts] = useState(product);
+
+  const value = { products, currency };
+
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
+
+export default ShopContextProvider;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./fonts/Vazir.ttf"
-import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./fonts/Vazir.ttf";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-)
+createRoot(document.getElementById("root")).render(
+  <ShopContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ShopContextProvider>
+);
