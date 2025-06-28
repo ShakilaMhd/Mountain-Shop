f805f5a6d2888467c1718624ecc2b992b9f7ea21
ShakilaMhd <shakila1996mhd@gmail.com>
2025-06-28 08:52:45 +0330

    add the login model & controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,10 +8,28 @@ const createToken = (id) => {
 }
 
 const loginUser = async (req, res) => {
+
     try {
+        const { email, password } = req.body
+
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ success: true, token })
+        } else {
+            res.json({ success: false, message: "Incorrect password" })
+        }
 
     } catch (error) {
-
+        console.log(error)
+        res.json({ success: false, message: error.message })    
     }
 }
 
