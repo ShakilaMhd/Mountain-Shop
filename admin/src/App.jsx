2a11f8eaa834c052e84bfcc78d64f652313164d0
ShakilaMhd <shakila1996mhd@gmail.com>
2025-07-05 19:37:32 +0330

    complete the List Page for my admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 
 // export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const backendUrl = "http://localhost:4000";
+export const currency = "تومان"
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token' || ''));
