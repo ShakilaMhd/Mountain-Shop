7d95b2aad95f58cd474229e7fb2445d135cf33ba
ShakilaMhd <shakila1996mhd@gmail.com>
2025-07-06 11:05:37 +0330

    add addToCart & updateCart && getUserCart

--- backend/controllers/cartControllers.js
+++ backend/controllers/cartControllers.js
@@ -29,7 +29,42 @@ const addToCart = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
-const updateCart = async (req, res) => { }
-const getUserCart = async (req, res) => { }
+const updateCart = async (req, res) => {
+
+    try {
+        const { userId, itemId, size, quantity } = req.body
+
+        const userData = await userModel.findById(userId)
+        let cartData = await userData.cartData
+
+        cartData[itemId][size] = quantity;
+
+        await userModel.findByIdAndUpdate(userId, { cartData });
+
+        res.json({ success: true, message: "کارت آپدیت شد" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
+const getUserCart = async (req, res) => {
+
+    try {
+        const { userId } = req.body
+        const userData = await userModel.findById(userId)
+
+        if (!userData) {
+            return res.json({ success: false, message: "کاربر پیدا نشد" })
+        }
+
+        const cartData = userData.cartData
+
+        res.json({ success: true, message: "کارت آپدیت شد" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 
 export { addToCart, updateCart, getUserCart }
